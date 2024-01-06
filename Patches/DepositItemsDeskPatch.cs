6471c18369a1efc18e66ef7a105787c1e2a2d9fe
Dade Lamkins <dade@lamkins.org>
2024-01-06 16:52:05 -0500

    First commit

---
+++ Patches/DepositItemsDeskPatch.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+
+namespace LCObligedCompany.Patches {
+    [HarmonyPatch(typeof(DepositItemsDesk))]
+    internal class DepositItemsDeskPatch {
+
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void BeNiceBeQuick(DepositItemsDesk __instance, ref List<GrabbableObject> ___itemsOnCounter, ref float ___grabObjectsTimer) {
+            if (__instance.IsHost) {
+                __instance.currentMood.desiresSilence = false;
+                __instance.currentMood.judgementSpeed = 0f;
+                __instance.currentMood.timeToWaitBeforeGrabbingItem = 0f;
+                __instance.currentMood.maxPlayersToKillBeforeSatisfied = 0;
+                __instance.currentMood.mustBeWokenUp = false;
+
+                ___grabObjectsTimer = Math.Min(3f, ___grabObjectsTimer);
+
+                __instance.patienceLevel = float.MaxValue;
+
+                if (___itemsOnCounter.Count > 0 && !__instance.doorOpen) { 
+                    __instance.OpenShutDoor(true);
+                }
+            }
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+﻿# No Bell Required
+
+The company accepts your items faster and without requiring you to ring the bell.
+- The door opens when items are placed on the table.
+- Items are taken after ~3 seconds.
+- The company is more patient.
+
+The company is happy to accept your junk.  :)
