8ab472ada7f05cba5506251fc41131ff460d6fb6
Dade Lamkins <dade@lamkins.org>
2024-01-07 17:07:45 -0500

    Updated icon and minor cleanup.

--- NoBellRequired.cs
+++ NoBellRequired.cs
@@ -4,21 +4,17 @@ using HarmonyLib;
 
 namespace LCObligedCompany {
     [BepInPlugin(MOD_NAMESPACE, MOD_NAME, MOD_VERSION)]
-    public class ObligedCompany : BaseUnityPlugin {
+    public class NoBellRequired : BaseUnityPlugin {
 
         private const string MOD_NAMESPACE = "freesnow.obligedcompany";
-        private const string MOD_NAME = "Obliged Company";
+        private const string MOD_NAME = "No Bell Required";
         private const string MOD_VERSION = "1.0.0";
 
         private readonly Harmony _harmony = new Harmony(MOD_NAMESPACE);
 
-        internal static ObligedCompany Instance { get; private set; }
-
         public ManualLogSource Logger { get; private set; }
 
         void Awake() {
-            if (Instance == null) Instance = this;
-
             this.Logger = BepInEx.Logging.Logger.CreateLogSource(MOD_NAMESPACE);
 
             this.Logger.LogInfo($"{MOD_NAME} v{MOD_VERSION} is now running.");
